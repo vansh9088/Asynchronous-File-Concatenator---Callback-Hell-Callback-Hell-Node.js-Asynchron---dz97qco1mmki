49456dd3b7d8136bbfff2b90148e2120790bc4eb
Vansh Sharma <vanshsharma@Vanshs-MacBook-Pro.local>
2025-09-03 22:28:17 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+const fs = require("fs/promises");
+
 async function cat(filePaths, outputFilePath) {
-  // write your code here
+  let output = [];
+
+  for (const path of filePaths) {
+    try {
+      const stats = await fs.stat(path);
+
+      if (stats.isFile()) {
+        const content = await fs.readFile(path, "utf-8");
+        output.push(content.trim());
+      } else if (stats.isDirectory()) {
+        output.push("Is a directory");
+      } else {
+        output.push("File not found");
+      }
+    } catch (err) {
+      output.push("File not found");
+    }
+  }
+
+  await fs.writeFile(outputFilePath, output.join("\n"));
 }
 
 module.exports = { cat };
